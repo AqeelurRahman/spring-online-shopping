ae966bf1722382c563f0b2b92425976f73fdb0cc
AqeelUrRahman <aqeel261@hotmail.com>
2019-07-24 10:56:06 +0500

    First Commit Adding maven quick starter

---
+++ shoppingbackend/src/main/java/com/burkit/App.java
@@ -0,0 +1,13 @@
+package com.burkit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
