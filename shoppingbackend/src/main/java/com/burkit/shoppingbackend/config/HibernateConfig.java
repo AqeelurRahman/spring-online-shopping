4eab252ab071e2dd7349cd32b0b3c002195e0318
AqeelUrRahman <aqeel261@hotmail.com>
2019-08-01 15:49:49 +0500

    SessionFactory problem solved + add one jar to project + dbcp.jar + all sets are working.....

--- shoppingbackend/src/main/java/com/burkit/shoppingbackend/config/HibernateConfig.java
+++ shoppingbackend/src/main/java/com/burkit/shoppingbackend/config/HibernateConfig.java
@@ -1,9 +1,5 @@
 package com.burkit.shoppingbackend.config;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,123 +7,59 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import static org.hibernate.cfg.AvailableSettings.*;
-import static org.hibernate.cfg.AvailableSettings.DIALECT;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
 
+import static org.hibernate.cfg.AvailableSettings.*;
 
 @Configuration
-@PropertySource("classpath:db.properties")
 @EnableTransactionManagement
-@ComponentScan(basePackages={"com.burkit.shoppingbackend.dto"})
+@PropertySource("classpath:db.properties")
 public class HibernateConfig {
     @Autowired
     private Environment env;
-
-    // Change the below based on the DBMS you choose
-    private final static String DATABASE_URL = "jdbc:mysql://localhost:3306/test";
-    private final static String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private final static String DATABASE_DIALECT = "MySql5Dialect";
-    private final static String DATABASE_USERNAME = "root";
-    private final static String DATABASE_PASSWORD = "";
-
-    // dataSource bean will be available
-    @Bean("dataSource")
-    public DataSource getDataSource() {
-
-        BasicDataSource dataSource = new BasicDataSource();
-
-        // Providing the database connection information
-        dataSource.setDriverClassName(DATABASE_DRIVER);
-        dataSource.setUrl(DATABASE_URL);
-        dataSource.setUsername(DATABASE_USERNAME);
-        dataSource.setPassword(DATABASE_PASSWORD);
-
-
-        return dataSource;
-
-    }
-
-    // sessionFactory bean will be available
-
-//    @Bean
-////    public SessionFactory getSessionFactory(DataSource dataSource) {
-////
-////        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
-////
-////        builder.addProperties(getHibernateProperties());
-////        builder.scanPackages("com.burkit.shoppingbackend.dto");
-////
-////        return builder.buildSessionFactory();
-////
-////    }
-@Bean
-public LocalSessionFactoryBean getSessionFactory() {
-    LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-
-    Properties props = new Properties();
-    // Setting JDBC properties
-    props.put(DRIVER, env.getProperty("mysql.driver"));
-    props.put(URL, env.getProperty("mysql.url"));
-    props.put(USER, env.getProperty("mysql.user"));
-    props.put(PASS, env.getProperty("mysql.password"));
-
-    // Setting Hibernate properties
-    props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
-    props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
-
-    // Setting C3P0 properties
-    props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
-    props.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
-    props.put(C3P0_ACQUIRE_INCREMENT,
-            env.getProperty("hibernate.c3p0.acquire_increment"));
-    props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
-    props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
-
-    props.put(DIALECT, env.getProperty("hibernate.dialect"));
-    factoryBean.setHibernateProperties(props);
-    factoryBean.setPackagesToScan("com.burkit.shoppingbackend.dto");
-
-    return factoryBean;
-}
-
-
-
-    // All the hibernate properties will be returned in this method
-//    private Properties getHibernateProperties() {
-//
-//        Properties properties = new Properties();
-//
-//
-//        properties.put("hibernate.dialect", DATABASE_DIALECT);
-//        properties.put("hibernate.show_sql", "true");
-//        properties.put("hibernate.format_sql", "true");
-//
-//        //properties.put("hibernate.hbm2ddl.auto", "create");
-//
-//
-//        return properties;
-//    }
-
-    // transactionManager bean
-//    @Bean
-//    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-//        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
-//        return transactionManager;
-//    }
-
     @Bean
-    public HibernateTransactionManager getTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(getSessionFactory().getObject());
-        return transactionManager;
+    public HibernateTemplate hibernateTemplate() {
+        return new HibernateTemplate(sessionFactory());
     }
-
-
+    @Bean
+    public SessionFactory sessionFactory() {
+        LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
+        lsfb.setDataSource(getDataSource());
+        lsfb.setPackagesToScan("com.burkit.shoppingbackend.dto");
+        lsfb.setHibernateProperties(hibernateProperties());
+        try {
+            lsfb.afterPropertiesSet();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lsfb.getObject();
+    }
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("mysql.driver"));
+        dataSource.setUrl(env.getProperty("mysql.url"));
+        dataSource.setUsername(env.getProperty("mysql.user"));
+        dataSource.setPassword(env.getProperty("mysql.password"));
+        return dataSource;
+    }
+    @Bean
+    public HibernateTransactionManager hibTransMan(){
+        return new HibernateTransactionManager(sessionFactory());
+    }
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        return properties;
+    }
 }
